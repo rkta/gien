3f5291c53a2b99ae2c8c77793ecd89503665f788
Jens John <dev@2ion.de>
2016-12-12 14:55:58 +0100

    0.4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "gien",
-        version = "0.4.0",
+        version = "0.4.1",
         packages = ["gien"],
 
         install_requires = [
